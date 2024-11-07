815fc87517939aed4af73b3a0798e0f1938ead48
chriselvam <christy.selvam@tum.de>
2024-11-07 22:21:38 +0100

    Add files via upload

---
+++ my_files.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IKIGAI
+# 
+# 
+
+# ## Introduction
+# This notebook demonstrates basic operations in data science, including working with different data science languages, libraries, and tools.
+
+# ## Data Science Languages
+# - Python
+# - R
+# - SQL
+# - Julia
+# 
+
+# ## Data Science Libraries
+# - Pandas
+# - NumPy
+# - Matplotlib
+# - Scikit-learn
+# 
+
+# ## Data Science Tools
+# | Tool            | Purpose                 |
+# |-----------------|-------------------------|
+# | Jupyter Notebooks| Interactive coding      |
+# | RStudio         | R programming IDE        |
+# | TensorFlow      | Machine learning library |
+# | GitHub          | Version control system   |
+# 
+
+# ## Arithmetic Expressions
+# In this section, we will demonstrate some basic arithmetic expressions in Python.
+# 
+
+# In[3]:
+
+
+a = 5
+b = 3
+multiplication = a * b
+addition = a + b
+multiplication, addition
+
+
+# In[4]:
+
+
+minutes = 150
+hours = minutes / 60
+hours
+
+
+# ## Objectives
+# - Learn about data science languages, libraries, and tools.
+# - Perform arithmetic operations.
+# - Convert time units (minutes to hours).
+# 
+
+# ## Author: Christy Selvam
+# 
+
+# In[ ]:
+
+
+
+
